cc326873f2e60a0f29b0564a6bf29ad74e16d26c
Lindsay Sauer <lindsay.sauer@gmail.com>
2022-02-24 09:12:21 -0500

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -18,23 +18,14 @@ module.exports = function(app, passport) {
         res.render('login.html', { message: req.flash('loginMessage') }); 
     });
 	
-	// process the login form
+    // process the login form
     app.post('/login', passport.authenticate('local-login', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/login', // redirect back to the login page if there is an error
-			failureFlash : true // allow flash messages
-		}),
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/login', // redirect back to the login page if there is an error
+		failureFlash : true // allow flash messages
+	}),
         function(req, res) {
-			// I don't see any hello. try testing later
-            console.log("hello");
-			/*
-            if (req.body.remember) {
-              req.session.cookie.maxAge = 1000 * 60 * 3;
-            } else {
-              req.session.cookie.expires = false;
-            }
-			*/
-			res.redirect('/profile');
+		res.redirect('/profile');
     });
 
     // =====================================
@@ -76,42 +67,7 @@ module.exports = function(app, passport) {
     // Heroes Roster
     // =====================================
 	app.get('/heroes', function(req, res) {
-		var page_name = "all_heroes";
-		/*
-		knex
-		  .select('HeroName', 'heroes.HeroGrade', 'HeroAttribute', 'HeroClass', 'HeroGradeName', 'HeroGradeIcon')
-		  .from('heroes')
-		  .join('hero_grade', function () {
-			this
-			  .on('hero_grade.HeroId', 'heroes.HeroId')
-			  .on('hero_grade.HeroGrade', 'heroes.HeroGrade');
-		  })
-		  .where('HeroStatus', 'Active')
-		  .orderBy('HeroName', 'Asc')
-		  .then(function (rows) { 
-			 console.log(rows);
-		  });
-		*/
-
-		
-  
-		/*
-		var qstr = 'Select HeroName, heroes.HeroGrade, HeroAttribute, HeroClass, HeroGradeName, HeroGradeIcon '+
-					'From `heroes` '+
-					'Inner Join hero_grade On hero_grade.HeroId = heroes.HeroId '+
-						'And hero_grade.HeroGrade = heroes.HeroGrade '+
-					'Where HeroStatus = ? Order By HeroName Asc';
-		var qstrValue = "Active";
-								
-		// Query all heroes
-		app.configDB.querySelect(qstr, qstrValue, function(rows) {
-			params = { page_name: page_name, user : req.user};
-			if (rows) {
-				params = { data: rows, page_name: page_name };
-			}
-			res.render(__dirname + "/heroes.html", params);		  
-		});			
-		*/	
+		var page_name = "all_heroes";  
 		var params = { data: [], page_name: page_name, user_data : req.user};
 		res.render("heroes.html", params);	
 	});
@@ -134,26 +90,10 @@ module.exports = function(app, passport) {
 		
 	});
 	
-    // =====================================
-    // Admin Functions
-    // =====================================
-	// Update a hero's database record
-	/*
-	app.get('/admin/heroes/update', function(req, res) {	
-		var valuePairs = {
-			'HeroName': 'Nia',
-			'HeroGrade': 5
-		};
-		var wherePairs = { 'HeroId': 1 };
-		
-	});
-	*/
 };
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-
-	//return next();//TAKE OUT LATER AND REPLACE WITH BELOW
 	
     // if user is authenticated in the session, carry on 
     if (req.isAuthenticated())
